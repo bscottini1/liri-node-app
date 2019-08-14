4ca5d4242520f5fae8c57824e865faddcf0dc080
Brett Scottini <bscottini1@gmail.com>
2019-08-13 23:03:14 -0400

    OMDB done

--- liri.js
+++ liri.js
@@ -1,1 +1,21 @@
 require("dotenv").config();
+const axios = require("axios");
+const Spotify = require("node-spotify-api");
+const dotenv = require("dotenv");
+const moment = require("moment");
+
+const movieName = process.argv.slice(3).join("+");
+const queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+axios.get(queryUrl).then(
+    function (response){
+        console.log(response.data.Title);
+        console.log(response.data.Year);
+        console.log(response.data.imdbRating);
+        console.log(response.data.Country);
+        console.log(response.data.Language);
+        console.log(response.data.Plot);
+        console.log(response.data.Actors);
+    });
+
+    
